44260b0375be55330a4bc4197eddbed12e3d8ae5
Madhon <madhon@madhon.co.uk>
2014-04-30 10:36:54 +0100

    updated fluentassertions

--- Ostrich/Properties/AssemblyInfo.cs
+++ Ostrich/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("83e2c1b0-82fc-44b7-a657-6216403446cc")]
 
 [assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.14062.1258")]
+[assembly: AssemblyFileVersion("1.0.14093.1333")]
