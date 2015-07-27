5b2998054af20e232512e37f5cc15b685c10534c
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-07-27 12:18:45 +0100

    Updated newstonsoft.json dependency

--- src/Ostrich/Properties/AssemblyInfo.cs
+++ src/Ostrich/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Resources;
 
 [assembly: Guid("83e2c1b0-82fc-44b7-a657-6216403446cc")]
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.8")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
